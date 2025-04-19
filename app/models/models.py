52a7b87e180825ac9518ac3b2f823d8d232e03a8
Malakhovskyy <malakhovskyy@outlook.com>
2025-04-19 21:11:45 +0300

    fixing

--- app/models/models.py
+++ app/models/models.py
@@ -1,14 +1,14 @@
 import sqlite3
 import os
 
-DB_PATH = os.path.join(os.path.dirname(__file__), "../../sshkeys.db")
-DB_PATH = os.path.abspath(DB_PATH)
+# Correct DB absolute path now
+DB_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../data/sshkeys.db"))
 
 def get_db_connection():
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
-
+    
 def init_db():
     conn = get_db_connection()
     c = conn.cursor()

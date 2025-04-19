bd6e132653fb528b1192a7d47f794d27e1b7bad5
Malakhovskyy <malakhovskyy@outlook.com>
2025-04-19 23:47:41 +0300

    fix

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -2,10 +2,10 @@
 
 if [ ! -f "/etc/letsencrypt/live/$DOMAIN/fullchain.pem" ]; then
   echo "[Startup] No certificates found for $DOMAIN. Starting HTTP nginx for challenge..."
-  cp /etc/nginx/conf.d/default.http.conf /etc/nginx/conf.d/default.conf
+  cp /etc/nginx/tpl/default.http.conf /etc/nginx/conf.d/default.conf
 else
   echo "[Startup] SSL certificates found for $DOMAIN. Starting full HTTPS nginx..."
-  cp /etc/nginx/conf.d/default.ssl.conf /etc/nginx/conf.d/default.conf
+  cp /etc/nginx/tpl/default.ssl.conf /etc/nginx/conf.d/default.conf
 fi
 
 # Start nginx

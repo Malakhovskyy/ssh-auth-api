88c6125b5e96a6587dbc8cdca67c4a259793d22a
Malakhovskyy <malakhovskyy@outlook.com>
2025-04-19 22:18:20 +0300

    fix startup

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem" ]; then
   echo "[Startup] SSL certificates not found, starting temporary HTTP-only nginx..."
-  cp /etc/nginx/conf.d/default.http.conf /etc/nginx/conf.d/default.conf
+  envsubst '${DOMAIN}' < /etc/nginx/conf.d/default.http.conf > /etc/nginx/conf.d/default.conf
 else
   echo "[Startup] SSL certificates found, starting full HTTPS nginx..."
   envsubst '${DOMAIN}' < /etc/nginx/conf.d/default.ssl.conf > /etc/nginx/conf.d/default.conf

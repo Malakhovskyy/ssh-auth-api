ee367d2f7dc995dc8a0be5e630dc38fdb88ec09d
Malakhovskyy <malakhovskyy@outlook.com>
2025-04-19 21:22:32 +0300

    fix

--- app/models/models.py
+++ app/models/models.py
@@ -1,10 +1,12 @@
 import sqlite3
 import os
 
-# Correct DB absolute path now
-DB_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../data/sshkeys.db"))
+DB_DIR = os.getenv("DB_DIR", "./data")  # <-- new variable, default ./data
+DB_PATH = os.path.join(DB_DIR, "sshkeys.db")
 
 def get_db_connection():
+    if not os.path.exists(DB_DIR):
+        os.makedirs(DB_DIR, exist_ok=True)
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn

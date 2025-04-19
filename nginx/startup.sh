3bb4af89b3a70c903f7958b19c5d615abe354a00
Malakhovskyy <malakhovskyy@outlook.com>
2025-04-19 22:58:48 +0300

    fix

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-DOMAIN=${DOMAIN}
+# No need to reassign DOMAIN manually, just use environment
 
-if [ -f "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem" ]; then
+if [ -f "/etc/letsencrypt/live/$DOMAIN/fullchain.pem" ]; then
   echo "[Startup] SSL certificates found. Starting full HTTPS nginx..."
   envsubst '$DOMAIN' < /etc/nginx/conf.d/default.ssl.conf > /etc/nginx/conf.d/default.conf
 else
@@ -10,4 +10,5 @@ else
   envsubst '$DOMAIN' < /etc/nginx/conf.d/default.http.conf > /etc/nginx/conf.d/default.conf
 fi
 
+# Start nginx
 nginx -g "daemon off;"

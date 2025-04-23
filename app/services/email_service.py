df2cfe92382e998cbf4f553ea364791949088886
Malakhovskyy <malakhovskyy@outlook.com>
2025-04-23 18:55:11 +0300

    fix

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -17,7 +17,7 @@ def send_backup_email(backup_path):
     admin_email = get_setting('smtp_from') or get_setting('smtp_user')
     queue_email(subject, f"Backup is available at {backup_path}", admin_email)
 
-def send_email(admin_email):
+def send_email(subject, body, admin_email):
     domain = get_setting('domain')  # Domain should also be stored in settings!
 #    reset_link = f"https://{domain}/admin/reset-password/{token}"
     subject = "SSH Key Manager - Password Reset"
